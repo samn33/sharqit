2262adc72ded192f5a14625c5df3cdabe046d1f6
samn33 <saminriver33@gmail.com>
2023-11-27 20:33:16 +0900

    add sample4.cpp

--- examples/build.sh
+++ examples/build.sh
@@ -2,3 +2,4 @@
 g++ -o sample1 -O4 -std=c++17 -L ~/lib -I ~/include -I /usr/include/eigen3 sample1.cpp -lsharq
 g++ -o sample2 -O4 -std=c++17 -L ~/lib -I ~/include -I /usr/include/eigen3 sample2.cpp -lsharq
 g++ -o sample3 -O4 -std=c++17 -L ~/lib -I ~/include -I /usr/include/eigen3 sample3.cpp -lsharq
+g++ -o sample4 -O4 -std=c++17 -L ~/lib -I ~/include -I /usr/include/eigen3 sample4.cpp -lsharq
